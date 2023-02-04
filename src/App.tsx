062651008c04d8b30e7e3a3c72dfc28ccda842c3
Abhinandan Panigrahi <chrzrdx@gmail.com>
2023-02-05 02:20:10 +0530

    Add rome for formatting and linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 function App() {
   return (
     <>
-      <header></header>
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
-      <footer></footer>
     </>
   );
 }
