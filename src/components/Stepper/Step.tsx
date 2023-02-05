1590741e24e34590c24556ff20f9a1b71fc1eee9
Abhinandan Panigrahi <chrzrdx@gmail.com>
2023-02-06 01:30:02 +0530

    First pass at animating the steps
    - Let framer motion apply css properties conditionally that we want to animate (instead of tailwind)
    - Set initial animation state to `false` as we don't want to animate on mount

--- src/components/Stepper/Step.tsx
+++ src/components/Stepper/Step.tsx
@@ -1,18 +1,28 @@
 import { TStepProps } from "../../types";
 import { CheckIcon } from "./CheckIcon";
-import clsx from "clsx";
+import { motion } from "framer-motion";
 
 export function Step({ step, active, inactive, completed }: TStepProps) {
   return (
-    <li
-      className={clsx(
-        "h-12 w-12 rounded-full border-2 flex items-center justify-center leading-none",
-        active && "border-blue-500 text-blue-600",
-        inactive && "border-slate-200 text-slate-400",
-        completed && "border-blue-500 bg-blue-500 text-white",
-      )}
+    <motion.li
+      className="h-12 w-12 rounded-full border-2 flex items-center justify-center leading-none"
+      initial={false}
+      animate={{
+        backgroundColor: completed
+          ? "var(--color-blue-500)"
+          : "var(--color-white)",
+        borderColor:
+          completed || active
+            ? "var(--color-blue-500)"
+            : "var(--color-slate-200)",
+        color: completed
+          ? "var(--color-white)"
+          : active
+          ? "var(--color-blue-600)"
+          : "var(--color-slate-400)",
+      }}
     >
       {completed ? <CheckIcon /> : step}
-    </li>
+    </motion.li>
   );
 }
