d7d109604d2e54ed4783b3cef6aa9a555eb71b4e
Abhinandan Panigrahi <chrzrdx@gmail.com>
2023-02-06 03:08:45 +0530

    Add ripple effect when a step is completed

--- src/components/Stepper/Step.tsx
+++ src/components/Stepper/Step.tsx
@@ -1,9 +1,9 @@
 import { STEP } from "../../enums";
 import { TStepProps } from "../../types";
 import { CheckIcon } from "./CheckIcon";
-import { motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
 
-const variants = {
+const stepVariants = {
   completed: {
     backgroundColor: "var(--color-blue-500)",
     borderColor: "var(--color-blue-500)",
@@ -19,28 +19,50 @@ const variants = {
     borderColor: "var(--color-slate-200)",
     color: "var(--color-slate-400)",
   },
-};
+} satisfies Variants;
+
+const backdropVariants = {
+  completed: {
+    scale: 1.25,
+    opacity: 0,
+    transition: {
+      duration: 0.6,
+    },
+  },
+  incomplete: {
+    scale: 1,
+    opacity: 1,
+  },
+} satisfies Variants;
 
 export function Step({ step, variant }: TStepProps) {
-  const stepVariant: keyof typeof variants = variant;
+  const stepVariant: keyof typeof stepVariants = variant;
   return (
-    <motion.li
-      className="h-12 w-12 rounded-full border-2 flex items-center justify-center leading-none"
-      variants={variants}
-      initial={false}
-      animate={stepVariant}
-    >
-      {variant === STEP.COMPLETED ? (
-        <div className="h-6 w-6">
-          <CheckIcon
-            initial={{ pathLength: 0 }}
-            animate={{ pathLength: 1 }}
-            transition={{ type: "tween", ease: "easeOut", delay: 0.2 }}
-          />
-        </div>
-      ) : (
-        step
-      )}
-    </motion.li>
+    <li className="h-12 w-12 relative">
+      <motion.div
+        className="absolute inset-0 bg-blue-300 rounded-full"
+        variants={backdropVariants}
+        initial="incomplete"
+        animate={stepVariant}
+      />
+      <motion.div
+        className="relative h-full w-full rounded-full border-2 flex items-center justify-center leading-none"
+        variants={stepVariants}
+        initial={false}
+        animate={stepVariant}
+      >
+        {variant === STEP.COMPLETED ? (
+          <div className="h-6 w-6">
+            <CheckIcon
+              initial={{ pathLength: 0 }}
+              animate={{ pathLength: 1 }}
+              transition={{ type: "tween", ease: "easeOut", delay: 0.2 }}
+            />
+          </div>
+        ) : (
+          step
+        )}
+      </motion.div>
+    </li>
   );
 }
