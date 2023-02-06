54c2410da95a2c75e89fca82435239b852e4358d
Abhinandan Panigrahi <chrzrdx@gmail.com>
2023-02-06 13:25:57 +0530

    Fix svg typo

--- src/components/Stepper/CheckIcon.tsx
+++ src/components/Stepper/CheckIcon.tsx
@@ -4,7 +4,7 @@ export function CheckIcon(props: MotionProps) {
   return (
     <svg
       width="100%"
-      height="auto"
+      height="100%"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
